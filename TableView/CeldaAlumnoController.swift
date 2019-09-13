96bf6f85d309650fb06d22507041b2f6ad024bfa
Alumno <ulsa@iMacD112.local>
2019-09-13 14:28:23 -0700

    TableViews

---
+++ TableView/CeldaAlumnoController.swift
@@ -0,0 +1,16 @@
+//
+//  CeldaAlumnoController.swift
+//  TableView
+//
+//  Created by Alumno on 9/13/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CeldaAlumnoController: UITableViewCell {
+    @IBOutlet weak var lblNombre: UILabel!
+    
+    
+}
